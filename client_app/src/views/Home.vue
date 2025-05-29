0b04f0663eb7928d1140bf7c4b57d2c4b2c81f4e
Yuxiang LI <yuxiang.li@shipt.com>
2025-05-29 00:21:29 -0400

    initial commit

---
+++ client_app/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to the User Home Page</h1>
+    <p>Logged in as: {{ username }}</p>
+    <p>User info: {{ user }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      username: localStorage.getItem('username') || 'Unknown',
+      user: JSON.parse(localStorage.getItem('user'))
+    }
+  }
+}
+</script>
