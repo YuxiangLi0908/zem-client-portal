8f70979ff904a7b7656575c41478e1b355cc4e3c
sangwei <1178424577@qq.com>
2025-06-17 16:59:21 +0800

    今日提交

--- client_app/src/views/Home.vue
+++ client_app/src/views/Home.vue
@@ -2,18 +2,7 @@
   <div class="login-wrapper">
     <MainHeader />
     <Slider />
-    <div class="flat-row bg-scheme pad-top0px pad-bottom0px" style="background-color: #32bfc0; color: white; width: 100%; padding: 20px 0;">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-12">
-                  <div class="promobox style1 style2 clearfix" style="display: flex; align-items: center; justify-content: space-between; padding-left: 300px; padding-right: 300px;">
-                      <h5 class="promobox-title" style="color: white; font-size: 1.5rem; margin: 0;">We are honored to be a leading and reliable partner in the field of transport in US</h5>
-                      <a class="button black sm" href="contact.html" style="background-color: black; color: white; padding: 8px 16px; white-space: nowrap;">contact us<i class="fa fa-chevron-right"></i></a>
-                  </div>
-              </div>
-          </div>
-      </div>
-    </div>
+    <SearchHeader />
 
     <ServiceShowcase />
     <div class="flat-row bg-scheme pad-top0px pad-bottom0px" style="background-color: #32bfc0; color: white; width: 100%; padding: 20px 0;">
@@ -34,12 +23,14 @@
 <script>
 import MainHeader from '@/components/layout/Header.vue'
 import Slider from '@/components/Slider.vue'
+import SearchHeader from '@/components/searchItem/SearchHeader.vue'
 import ServiceShowcase from '@/components/layout/ServiceShowcase.vue'
 export default {
   name: 'App',
   components: {
     MainHeader,
     Slider,
+    SearchHeader,
     ServiceShowcase,
   },
   mounted() {
