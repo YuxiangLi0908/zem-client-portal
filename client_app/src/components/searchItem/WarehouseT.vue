078576ffde3ee1c8e43ebdbe2e1fc4168c8c5e94
sangwei <1178424577@qq.com>
2025-07-01 17:39:38 +0800

    修改显示的预约时间和显示的预约批次

--- client_app/src/components/searchItem/WarehouseT.vue
+++ client_app/src/components/searchItem/WarehouseT.vue
@@ -57,7 +57,7 @@
                 <template v-if="status === 'shipped'">
                   <div class="time-item">
                     <div class="time-label">预约</div>
-                    <div class="time-value">{{ formatTime(item.shipment_schduled_at) }}</div>
+                    <div class="time-value">{{ formatTime(item.shipment_appointment) }}</div>
                   </div>
                   <div class="time-separator">→</div>
                   <div class="time-item highlight">
@@ -70,7 +70,7 @@
                 <template v-else-if="status === 'arrived'">
                   <div class="time-item">
                     <div class="time-label">预约</div>
-                    <div class="time-value">{{ formatTime(item.shipment_schduled_at) }}</div>
+                    <div class="time-value">{{ formatTime(item.shipment_appointment) }}</div>
                   </div>
                   <div class="time-separator">→</div>
                   <div class="time-item">
@@ -88,7 +88,7 @@
                 <template v-else-if="status === 'pod_uploaded'">
                     <div class="time-item">
                         <div class="time-label">预约</div>
-                        <div class="time-value">{{ formatTime(item.shipment_schduled_at) }}</div>
+                        <div class="time-value">{{ formatTime(item.shipment_appointment) }}</div>
                     </div>
                     <div class="time-separator">→</div>
                     <div class="time-item">
@@ -115,7 +115,7 @@
                 <template v-else-if="status === 'scheduled'">
                   <div class="time-item highlight">
                     <div class="time-label">预约</div>
-                    <div class="time-value">{{ formatTime(item.shipment_schduled_at) }}</div>
+                    <div class="time-value">{{ formatTime(item.shipment_appointment) }}</div>
                   </div>
                 </template>
               </div>
@@ -177,7 +177,7 @@ export default {
           groups.arrived.push(item)
         } else if (item.is_shipped) {
           groups.shipped.push(item)
-        } else if (item.shipment_batch_number) {
+        } else if (item.master_shipment_batch_number) {
           groups.scheduled.push(item)
         } else {
           groups.unscheduled.push(item)
